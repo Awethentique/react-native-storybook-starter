f52caf373312d9f461163e25840b147fa3e26dea
Awethentique <ritesh.makan@gmail.com>
2023-04-11 22:14:49 +0200

    update main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,19 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-react-native-web',
+     {
+      name: '@storybook/addon-react-native-web',
+      options: {
+        modulesToTranspile: [
+          '@ant-design/react-native',
+          '@react-native-camera-roll/camera-roll', 
+          '@react-native-picker/picker',
+          '@react-native-community/segmented-control',
+          '@react-native-community/slider',
+          'react-native-gesture-handler',
+        ],
+      },
+    },
   ],
   framework: '@storybook/react',
   core: {
