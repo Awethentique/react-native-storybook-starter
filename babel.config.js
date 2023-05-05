b3e510101e049f146a10eb67e1cb4e75c3c16990
Awethentique <ritesh.makan@gmail.com>
2023-05-05 17:10:09 +0200

    Fixed addon not working with react-native library

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.ios.js', '.android.js'],
+        root: ['./src'],
+        alias: {
+          '~': './src',
+        },
+      },
+      'root-alias',
+    ],
+    [
+      'import',
+      {
+        libraryName: '@ant-design/react-native',
+      },
+    ],
+  ],
 };
--- components/AntButton/AntButton.stories.tsx
+++ components/AntButton/AntButton.stories.tsx
@@ -1,13 +1,22 @@
 // stories/AntButton.stories.tsx
-
+import React from 'react';
 import {ComponentMeta, StoryObj} from '@storybook/react';
 
-import {Button as AntButton} from '@ant-design/react-native';
+import {Button} from '@ant-design/react-native';
+import type {ButtonProps} from '@ant-design/react-native/es/button/index';
+
+import {View} from 'react-native';
 
 export default {
   title: 'components/AntButton',
   argTypes: {onPress: {action: 'pressed'}},
-  component: AntButton,
-} as ComponentMeta<typeof AntButton>;
+  component: Button,
+} as ComponentMeta<typeof Button>;
 
-export const Basic = () => <AntButton>Click me</AntButton>;
+export const Basic: StoryObj<ButtonProps> = () => {
+  return (
+    <View>
+      <Button type="primary">Click me</Button>
+    </View>
+  );
+};
